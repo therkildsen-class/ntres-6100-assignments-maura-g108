dd11ebb65363b56edad883a2d0799fae71f71cf1
maura-g108 <maura.gallagher108@gmail.com>
2025-09-16 10:38:59 -0400

    ggplot notes

--- course_notes/ggplot_intro.R
+++ course_notes/ggplot_intro.R
@@ -38,3 +38,20 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_wrap(~ class)
+
+
+
+
+
+
+
+plot1 <- ggplot(data=mpg, mapping = aes(x=displ, y=hwy)) +
+  geom_point(mapping=aes (color=class, size=cyl), shape=1) +
+  geom_smooth() +
+  facet_wrap(~year, nrow=2) +
+  theme_minimal()
+
+plot1
+
+ggsave("course_notes/plots/hwy_vs_displ.png", plot1, width = 8, height = 4)
+?ggsave
